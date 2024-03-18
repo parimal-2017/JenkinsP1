32760e04c4a8db614b6b6f6b8c254eb1d9333bb9
parimal-2017 <149002295+parimal-2017@users.noreply.github.com>
2024-03-18 17:56:51 +0530

    first change

--- src/test/java/basic/OrganizationTest.java
+++ src/test/java/basic/OrganizationTest.java
@@ -1,5 +1,8 @@
 package basic;
 
+import java.io.IOException;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -8,7 +11,12 @@ public class OrganizationTest {
 	@Test
 	public void CreateOrganizationTest() {
 		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
 		driver.get("http://localhost:8888/");
+		//log in to application
+		driver.findElement(By.name("user_name")).sendKeys("admin");
+		driver.findElement(By.name("user_password")).sendKeys("admin");
+		driver.findElement(By.id("submitButton")).click();
 		
 		
 	}
